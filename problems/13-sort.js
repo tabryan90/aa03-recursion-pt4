741bf9b1a06e02a0b443a5af959ddb72acda59c8
Alex Bryan <tabryan1990@gmail.com>
2024-02-24 15:48:24 -0500

    problem 13 walkthrough added

--- problems/13-sort.js
+++ problems/13-sort.js
@@ -20,9 +20,36 @@ sort([4,1,6,3,1,7]); // [1, 1, 3, 4, 6, 7]
 sort([0, 1, -3]); // [-3, 0, 1]
 sort([]); // []
 ***********************************************************************/
+debugger
+// function sort(nums) {
+//     if (!nums.length) return nums;
+
+//     const [p, ...rest] = nums
+
+//     const low = rest.filter(n => n <= p)
+//     const high = rest.filter(n => n > p)
+
+//     return [...sort(low), p, ...sort(high)]
+// }
 
 function sort(nums, sorted = []) {
-    // Your code here 
+    if (nums.length === 0) {
+        return sorted;
+    }
+
+    let minIndex = 0;
+
+    for ( let i = 0; i < nums.length; i++ ) {
+        if (nums[i] < nums[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    sorted.push(nums[minIndex]);
+    nums.splice(minIndex, 1);
+
+    return sort(nums, sorted);
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
